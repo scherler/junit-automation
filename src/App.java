fdfe77c286a21fa37dc5a32d0968606bc564100f
Thorsten Scherler <scherler@gmail.com>
2023-09-22 11:37:20 +0200

    [scherler-patch-1] add debug tests
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

--- src/App.java
+++
@@ -1,8 +0,0 @@
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Car myFerrari = new Car("Ferrari","488"); 
-        myFerrari.moveForward();
-        myFerrari.brake();
-    }
-}
